b8d2d4f776ce6cb8cb1d09ed39e3f5eb47c00040
SystemsJava76 <52467247+SystemsJava76@users.noreply.github.com>
2019-09-07 15:47:23 -0500

    Add files via upload

---
+++ PrimerVideo.java
@@ -0,0 +1,15 @@
+
+package primervideo;
+
+
+public class PrimerVideo {
+
+
+    public static void main(String[] args) {
+     
+        carro bochito=new carro();
+        
+        bochito.encender();
+    }
+    
+}
---
+++ carro.java
@@ -0,0 +1,27 @@
+
+
+package primervideo;
+
+public class carro {
+    
+    public carro(){
+        //constructor
+        System.out.println("Se ha construido el objeto carro");
+    }
+    
+    public void encender(){
+        System.out.println("Estoy encendido");
+    }
+    
+    public void frenar(){
+        System.out.println("Estoy frenando");
+    }
+    public void apagar(){
+        System.out.println("Estoy apagando");
+    }
+    public void avanzar(){
+        System.out.println("Estoy avanzando");
+        
+    }
+    
+}
